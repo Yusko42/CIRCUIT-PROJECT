76f9b1440010bd519f6b38d6ec30eeed0eae3b2e
Yus42 <144340044+Yusko42@users.noreply.github.com>
2024-05-31 02:01:47 +0700

    Update speed_measure.sv
    unnecessary comments deleted

--- speed_measure.sv
+++ speed_measure.sv
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 25.05.2024 21:29:29
-// Design Name: 
-// Module Name: speed_measure
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 module speed_measure(
     input logic clk,           // 12 MHz clock
@@ -27,7 +8,7 @@ module speed_measure(
     output logic [6:0] seg2,
     output logic [6:0] seg3,
     output logic [6:0] seg4,
-    output logic dot            //Точка на UL2
+    output logic dot 
 );
 
     logic [23:0] count = 24'd0;
@@ -36,15 +17,7 @@ module speed_measure(
     logic sensor_b_last = 0;
     logic in_process = 0;
 
-    // Счетчик времени
-    always_ff @(posedge clk) begin
-        //if (rst) begin
-            //count <= 24'd0;
-            //time_counted <= 24'd0;
-            //sensor_a_last <= 0;
-            //sensor_b_last <= 0;
-            //in_process <= 0;
-        sensor_a_last <= sensor_a;
+    always_ff @(posedge clk) begina;
         sensor_b_last <= sensor_b;
         if (sensor_a && !sensor_a_last) begin
             in_process <= 1;
@@ -57,7 +30,6 @@ module speed_measure(
         end
     end
 
-    // Вычисление скорости
     logic [13:0] speed_calc;
     always_comb begin
         if (time_counted != 0) //12 MHz * 300 mm
@@ -66,7 +38,6 @@ module speed_measure(
             speed_calc = 0;
     end
 
-    // Дисплей
     SevenSegmentDisplay display (
         .clk(clk),
         .value(speed_calc),
@@ -124,63 +95,4 @@ module SevenSegmentDisplay(
         dot = 1;
     end
     
-endmodule    
-    
-    
-//        case (digit[0])
-//            4'h0: seg1 = 7'b0111111;
-//            4'h1: seg1 = 7'b0000110;
-//            4'h2: seg1 = 7'b1011011;
-//            4'h3: seg1 = 7'b1001111;
-//            4'h4: seg1 = 7'b1100110;
-//            4'h5: seg1 = 7'b1101101;
-//            4'h6: seg1 = 7'b1111101;
-//            4'h7: seg1 = 7'b0000111;
-//            4'h8: seg1 = 7'b1111111;
-//            4'h9: seg1 = 7'b1101111;
-//            default: seg1 = 7'b0000000;
-//        endcase
-
-//        case (digit[1])
-//            4'h0: seg2 = 7'b0111111;
-//            4'h1: seg2 = 7'b0000110;
-//            4'h2: seg2 = 7'b1011011;
-//            4'h3: seg2 = 7'b1001111;
-//            4'h4: seg2 = 7'b1100110;
-//            4'h5: seg2 = 7'b1101101;
-//            4'h6: seg2 = 7'b1111101;
-//            4'h7: seg2 = 7'b0000111;
-//            4'h8: seg2 = 7'b1111111;
-//            4'h9: seg2 = 7'b1101111;
-//            default: seg2 = 7'b0000000;
-//        endcase
-
-//        case (digit[2])
-//            4'h0: seg3 = 7'b0111111;
-//            4'h1: seg3 = 7'b0000110;
-//            4'h2: seg3 = 7'b1011011;
-//            4'h3: seg3 = 7'b1001111;
-//            4'h4: seg3 = 7'b1100110;
-//            4'h5: seg3 = 7'b1101101;
-//            4'h6: seg3 = 7'b1111101;
-//            4'h7: seg3 = 7'b0000111;
-//            4'h8: seg3 = 7'b1111111;
-//            4'h9: seg3 = 7'b1101111;
-//            default: seg3 = 7'b0000000;
-//        endcase
-        
-//        case (digit[3])
-//            4'h0: seg4 = 7'b0111111;
-//            4'h1: seg4 = 7'b0000110;
-//            4'h2: seg4 = 7'b1011011;
-//            4'h3: seg4 = 7'b1001111;
-//            4'h4: seg4 = 7'b1100110;
-//            4'h5: seg4 = 7'b1101101;
-//            4'h6: seg4 = 7'b1111101;
-//            4'h7: seg4 = 7'b0000111;
-//            4'h8: seg4 = 7'b1111111;
-//            4'h9: seg4 = 7'b1101111;
-//            default: seg4 = 7'b0000000;
-//        endcase
-
-
+endmodule
